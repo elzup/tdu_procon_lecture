ff1889df068f462a2847040718efbda91ac04bb6
elzup <guild0105@gmail.com>
2016-06-06 21:37:14 +0900

    Add: chap4 [WIP]

---
+++ cpp/chap04_binary_search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+int n, q, a[10000];
+
+bool binary_search_contains(int v) {
+    int l = 0; // left
+    int r = n - 1; // right
+    while (l <= r) {
+        int m = (r + l) / 2;
+        if (a[m] == v) {
+            return true;
+        }
+        if (a[m] > v) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cin >> q;
+    int c = 0;
+    for (int i = 0; i < q; i++) {
+        int t;
+        cin >> t;
+        // original
+        // c += binary_search_contains(t) ? 1 : 0;
+
+        // binary_search
+        c += binary_search(a, a + n, t) ? 1 : 0;
+    }
+
+    cout << c << endl;
+}
---
+++ cpp/chap04_liner_search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, q, s[10000];
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    cin >> q;
+    int c = 0;
+    for (int i = 0; i < q; i++) {
+        int t;
+        cin >> t;
+        for (int j = 0; j < n; j++) {
+            if (t == s[j]) {
+                c++;
+                break;
+            }
+        }
+    }
+
+    cout << c << endl;
+}
